45dda4f3c1788e7fc13da8ececfb8115209631c4
tsayvyac <slava.tsay23@gmail.com>
2024-05-16 12:26:19 +0200

    GoodScoreStrategy was updated. Added profiles for starting application.

--- src/main/java/com/tsayvyac/flashcard/service/impl/strategy/GoodScoreStrategy.java
+++ src/main/java/com/tsayvyac/flashcard/service/impl/strategy/GoodScoreStrategy.java
@@ -1,24 +1,38 @@
 package com.tsayvyac.flashcard.service.impl.strategy;
 
 import com.tsayvyac.flashcard.model.Progress;
+import lombok.extern.slf4j.Slf4j;
 
 import java.time.LocalDate;
 
-import static com.tsayvyac.flashcard.util.Constant.*;
-
+@Slf4j(topic = "GOOD_SCORE_LOG")
 public class GoodScoreStrategy implements ComputeProgressStrategy {
 
     @Override
     public Progress computeDate(Progress existing) {
-        int daysToAdd = (int) (Math.max(1, AFTER_DAYS * existing.getStreak() * REPETITION_COEFFICIENT));
-        if (daysToAdd > MAX_DAYS) daysToAdd = MAX_DAYS;
+        int daysToAdd = numberFromFibByPos(existing.getStreak() + 1);
         LocalDate nextD = LocalDate.now().plusDays(daysToAdd);
-
+        log.info("Days to add: {}", daysToAdd);
         return Progress.builder()
                 .repetitions(existing.getRepetitions() + 1)
                 .streak(existing.getStreak() + 1)
                 .lastScore(1)
                 .nextDate(nextD)
                 .build();
+    }
+
+    private int numberFromFibByPos(int pos) {
+        if (pos == 1 || pos == 0) return 1;
+        if (pos >= 9) return 34;
+        else {
+            int prev = 0;
+            int current = 1;
+            for (int i = 2; i <= pos; i++) {
+                int temp = current;
+                current += prev;
+                prev = temp;
+            }
+            return current;
+        }
     }
 }
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,48 @@
+
+CREATE SEQUENCE IF NOT EXISTS card_set_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE IF NOT EXISTS flashcard_seq START WITH 1 INCREMENT BY 1;
+
+CREATE SEQUENCE IF NOT EXISTS learner_seq START WITH 1 INCREMENT BY 1;
+
+CREATE TABLE IF NOT EXISTS card_set
+(
+    id         BIGINT       NOT NULL,
+    name       VARCHAR(255) NOT NULL,
+    learner_id BIGINT       NOT NULL,
+    CONSTRAINT pk_cardset PRIMARY KEY (id),
+    CONSTRAINT FK_CARDSET_ON_LEARNER FOREIGN KEY (learner_id) REFERENCES learner (uid)
+);
+
+CREATE TABLE IF NOT EXISTS flashcard
+(
+    id     BIGINT NOT NULL,
+    front  OID    NOT NULL,
+    back   OID    NOT NULL,
+    set_id BIGINT NOT NULL,
+    CONSTRAINT pk_flashcard PRIMARY KEY (id),
+    CONSTRAINT FK_FLASHCARD_ON_SET FOREIGN KEY (set_id) REFERENCES card_set (id) ON DELETE CASCADE
+);
+
+CREATE TABLE IF NOT EXISTS learner
+(
+    uid      BIGINT       NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    email    VARCHAR(255) NOT NULL,
+    role     VARCHAR(255),
+    CONSTRAINT pk_learner PRIMARY KEY (uid)
+);
+
+CREATE TABLE IF NOT EXISTS progress
+(
+    flashcard_id BIGINT  NOT NULL,
+    repetitions  INTEGER NOT NULL,
+    streak       INTEGER NOT NULL,
+    last_score   INTEGER,
+    next_date    date    NOT NULL,
+    learner_id   BIGINT  NOT NULL,
+    CONSTRAINT pk_progress PRIMARY KEY (flashcard_id),
+    CONSTRAINT FK_PROGRESS_ON_FLASHCARD FOREIGN KEY (flashcard_id) REFERENCES flashcard (id) ON DELETE CASCADE,
+    CONSTRAINT FK_PROGRESS_ON_LEARNER FOREIGN KEY (learner_id) REFERENCES learner (uid)
+);
